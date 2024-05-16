62a3556e3b4774eed695292b2abfa4dc68f5109f
jee-yann <gian.alexavier@gmail.com>
2024-05-17 10:04:14 +1200

    Fixed Medium AI so that the first 3 rounds use random strategy before it switches to top strategy

--- src/main/java/nz/ac/auckland/se281/MediumAI.java
+++ src/main/java/nz/ac/auckland/se281/MediumAI.java
@@ -8,11 +8,17 @@ import java.util.ArrayList;
 
 public class MediumAI extends AI {
   public MediumAI(Choice choice) {
-    super(new TopStrategy(), choice);
+    super(new RandomStrategy(), choice);
   }
 
   @Override
   public int getMove(List<Choice> history) {
+
+    // Check if round 3, if so, switch from random strategy to top strategy.
+    if (rounds == 3) {
+      this.setStrategy(new TopStrategy());
+    }
+    rounds++;
     return this.strategy.getNumber(history, this.choice);
   }
 }
