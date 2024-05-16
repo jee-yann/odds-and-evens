0dec5bb15d9974d9dda192a36b4b02115b5790e9
jee-yann <gian.alexavier@gmail.com>
2024-05-17 06:01:49 +1200

    Added printing functionality to show user's hand

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -5,15 +5,20 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
+  
+  private String name;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    this.name = options[0];
   }
 
   public void play() {
     MessageCli.START_ROUND.printMessage("1");
     MessageCli.ASK_INPUT.printMessage();
+    String input = Utils.scanner.nextLine();
+    MessageCli.PRINT_INFO_HAND.printMessage(this.name, input);
   }
 
   public void endGame() {}
