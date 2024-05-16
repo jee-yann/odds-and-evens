bc7995faf40ea0c72f6f0b496edc7312cf8f55ad
jee-yann <gian.alexavier@gmail.com>
2024-05-17 08:39:09 +1200

    Added a message at the end of the round to show who wins

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -9,15 +9,19 @@ public class Game {
   private String name;
   private int round = 1;
   private Difficulty difficulty;
+  private Choice choice;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
     this.name = options[0];
     this.difficulty = difficulty;
+    this.choice = choice;
   }
 
   public void play() {
+    
+
     MessageCli.START_ROUND.printMessage(Integer.toString(this.round));
     MessageCli.ASK_INPUT.printMessage();
     String input = Utils.scanner.nextLine();
@@ -26,13 +30,34 @@ public class Game {
       MessageCli.ASK_INPUT.printMessage();
       input = Utils.scanner.nextLine();
     }
+    int humanMove = Integer.parseInt(input);
     
     AI ai = AIFactory.createAI(difficulty);
-    
-    MessageCli.PRINT_INFO_HAND.printMessage("HAL-9000", Integer.toString(ai.getMove()));
-
+    int aiMove = ai.getMove();
+    String aiName = "HAL-9000";
 
     MessageCli.PRINT_INFO_HAND.printMessage(this.name, input);
+    MessageCli.PRINT_INFO_HAND.printMessage(aiName, Integer.toString(aiMove));
+
+
+    int sum = humanMove + aiMove;
+    String winner;
+    if ((Utils.isEven(sum) && choice == Choice.EVEN) || 
+        (Utils.isOdd(sum) && choice == Choice.ODD)) {
+      winner = this.name;
+    } else {
+      winner = aiName;
+    }
+
+    String evenOrOdd;
+    if (Utils.isOdd(sum)) {
+      evenOrOdd = "ODD";
+    } else {
+      evenOrOdd = "EVEN";
+    }
+
+    MessageCli.PRINT_OUTCOME_ROUND.printMessage(Integer.toString(sum), evenOrOdd, winner);
+
 
     round++;
 
